9d11abfcd22c276e8b4459e29020df07999b5e87
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-28 11:36:32 -0700

    setup grunt init, loadNpmTasks and registerTask. Install 2015-presets. enable presets in babelrc. reference dist/es5 in index

--- gruntfile.js
+++ gruntfile.js
@@ -2,5 +2,27 @@ module.exports = function (grunt) {
     'strict mode';
 
     // initConfig to specify config for babel
-    grunt.initConfig()
+    grunt.initConfig({
+      babel: {
+        options: {
+          sourceMap: true
+        },
+        dist: {
+          files: {
+            // output file that is compiled key
+            // input is the es6
+            'dist/es5.js': 'js/es6.js'
+          }
+        }
+      }
+    });
+
+    // load babel
+    grunt.loadNpmTasks('grunt-babel');
+    // register so that it can run
+    // alias es6 on command line that will run babel task
+    grunt.registerTask('es6', ['babel']); 
+    // need to enable presets or plugins to run the code npm install babel-preset-es2015 and transform each feature
+    // babelrc enable the preset that was installed
+
 };
